1dbfc45b8c56774dc63d2905477f81883a516e3c
louislzcute <ll.khac.quan@gmail.com>
2016-04-28 14:40:19 +0700

    Update
    update last commit

--- Custom Cover/Program.cs
+++ Custom Cover/Program.cs
@@ -16,7 +16,7 @@ namespace Custom_Cover
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CoverMakeForm());
         }
     }
 }
